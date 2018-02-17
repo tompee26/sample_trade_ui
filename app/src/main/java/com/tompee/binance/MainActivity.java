0b9b4eaab81ae2cc0567a338cea6ffe8a8e5f207
tompee <tompee@alink-group.co.jp>
2018-02-17 22:04:25 +0900

    Attempted depth listening

--- app/src/main/java/com/tompee/binance/MainActivity.java
+++ app/src/main/java/com/tompee/binance/MainActivity.java
@@ -1,11 +1,8 @@
 package com.tompee.binance;
 
 import android.databinding.DataBindingUtil;
-import android.os.AsyncTask;
 import android.os.Bundle;
-import android.os.Handler;
 import android.support.v4.app.FragmentActivity;
-import android.support.v4.view.ViewPager;
 import android.view.View;
 
 import com.tompee.binance.controller.adapter.MarketPageAdapter;
@@ -13,31 +10,25 @@ import com.tompee.binance.databinding.ActivityMainBinding;
 import com.tompee.binance.model.MarketItem;
 import com.tompee.binance.model.SortManager;
 
-import java.util.Collections;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
-public class MainActivity extends FragmentActivity implements View.OnClickListener,
-        ViewPager.OnPageChangeListener {
+public class MainActivity extends FragmentActivity implements View.OnClickListener {
     public static final Map<String, MarketItem> tokenMap = new HashMap<>();
+    public static final SortManager sortManager = new SortManager();
 
     private ActivityMainBinding mBinding;
-    private SortManager mSortManager;
-    private MarketPageAdapter mMarketPageAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        mSortManager = new SortManager();
         mBinding = DataBindingUtil.setContentView(this,
                 R.layout.activity_main);
-        mBinding.setSortManager(mSortManager);
+        mBinding.setSortManager(sortManager);
         mBinding.tabLayoutMain.setupWithViewPager(mBinding.viewPager);
-        mMarketPageAdapter = new MarketPageAdapter(this, getSupportFragmentManager());
-        mBinding.viewPager.addOnPageChangeListener(this);
-        mBinding.viewPager.setOffscreenPageLimit(mMarketPageAdapter.getCount());
-        mBinding.viewPager.setAdapter(mMarketPageAdapter);
+        MarketPageAdapter marketPageAdapter = new MarketPageAdapter(this, getSupportFragmentManager());
+        mBinding.viewPager.setOffscreenPageLimit(1);
+        mBinding.viewPager.setAdapter(marketPageAdapter);
 
         onClick(mBinding.vol);
         mBinding.pair.setOnClickListener(this);
@@ -50,94 +41,33 @@ public class MainActivity extends FragmentActivity implements View.OnClickListen
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.pair:
-                mSortManager.changeToPairVol(SortManager.SortType.PAIR);
-                mBinding.pair.setSelected(mSortManager.getSortType() == SortManager.SortType.PAIR);
-                mBinding.vol.setSelected(mSortManager.getSortType() == SortManager.SortType.VOL);
+                sortManager.changeToPairVol(SortManager.SortType.PAIR);
+                mBinding.pair.setSelected(sortManager.getSortType() == SortManager.SortType.PAIR);
+                mBinding.vol.setSelected(sortManager.getSortType() == SortManager.SortType.VOL);
                 mBinding.ltp.setSelected(false);
                 mBinding.percentChange.setSelected(false);
                 break;
             case R.id.vol:
-                mSortManager.changeToPairVol(SortManager.SortType.VOL);
-                mBinding.pair.setSelected(mSortManager.getSortType() == SortManager.SortType.PAIR);
-                mBinding.vol.setSelected(mSortManager.getSortType() == SortManager.SortType.VOL);
+                sortManager.changeToPairVol(SortManager.SortType.VOL);
+                mBinding.pair.setSelected(sortManager.getSortType() == SortManager.SortType.PAIR);
+                mBinding.vol.setSelected(sortManager.getSortType() == SortManager.SortType.VOL);
                 mBinding.ltp.setSelected(false);
                 mBinding.percentChange.setSelected(false);
                 break;
             case R.id.ltp:
-                mSortManager.changeToLastPrice();
+                sortManager.changeToLastPrice();
                 mBinding.pair.setSelected(false);
                 mBinding.vol.setSelected(false);
                 mBinding.ltp.setSelected(true);
                 mBinding.percentChange.setSelected(false);
                 break;
             case R.id.percentChange:
-                mSortManager.changeToPercentChanged();
+                sortManager.changeToPercentChanged();
                 mBinding.pair.setSelected(false);
                 mBinding.vol.setSelected(false);
                 mBinding.ltp.setSelected(false);
                 mBinding.percentChange.setSelected(true);
                 break;
-        }
-        onPageSelected(mBinding.viewPager.getCurrentItem());
-    }
-
-    @Override
-    public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
-
-    }
-
-    @Override
-    public void onPageSelected(int position) {
-    }
-
-    @Override
-    public void onPageScrollStateChanged(int state) {
-
-    }
-
-    class SortTask extends AsyncTask<Void, Void, Void> {
-        private final List<MarketItem> mMarketItemList;
-        private final int mPosition;
-
-        SortTask(List<MarketItem> marketItemList, int position) {
-            mMarketItemList = marketItemList;
-            mPosition = position;
-        }
-
-        @Override
-        protected Void doInBackground(Void... voids) {
-            switch (mSortManager.getSortType()) {
-                case PAIR:
-                    Collections.sort(mMarketItemList, (l1, l2) -> l1.getTokenName().compareTo(l2.getTokenName()));
-                    break;
-                case VOL:
-                    Collections.sort(mMarketItemList, (l1, l2) -> -compareDouble(l1.getVolume(), l2.getVolume()));
-                    break;
-                case LAST_PRICE_ASCENDING:
-                    Collections.sort(mMarketItemList, (l1, l2) -> compareDouble(l1.getPrice(), l2.getPrice()));
-                    break;
-                case LAST_PRICE_DESCENDING:
-                    Collections.sort(mMarketItemList, (l1, l2) -> -compareDouble(l1.getPrice(), l2.getPrice()));
-                    break;
-                case PERCENT_CHANGE_ASCENDING:
-                    Collections.sort(mMarketItemList, (l1, l2) -> compareDouble(l1.getChange(), l2.getChange()));
-                    break;
-                case PERCENT_CHANGE_DESCENDING:
-                    Collections.sort(mMarketItemList, (l1, l2) -> -compareDouble(l1.getChange(), l2.getChange()));
-                    break;
-            }
-            return null;
-        }
-
-        @Override
-        protected void onPostExecute(Void aVoid) {
-            new Handler().postDelayed(() -> mMarketPageAdapter.sort(mPosition), 200);
-        }
-
-        private int compareDouble(double d1, double d2) {
-            if (d1 > d2) return 1;
-            if (d1 < d2) return -1;
-            return 0;
         }
     }
 }
--- app/src/main/java/com/tompee/binance/services/api/BinanceWrapper.java
+++ app/src/main/java/com/tompee/binance/services/api/BinanceWrapper.java
@@ -2,10 +2,12 @@ package com.tompee.binance.services.api;
 
 import android.content.Context;
 
-import com.binance.api.client.BinanceApiAsyncRestClient;
+import com.binance.api.client.BinanceApiCallback;
 import com.binance.api.client.BinanceApiClientFactory;
-import com.binance.api.client.BinanceApiRestClient;
+import com.binance.api.client.BinanceApiWebSocketClient;
 import com.binance.api.client.constant.BinanceApiConstants;
+import com.binance.api.client.domain.event.DepthEvent;
+import com.binance.api.client.exception.BinanceApiException;
 import com.tompee.binance.R;
 import com.tompee.binance.model.AllMarketTickersEvent;
 
@@ -20,14 +22,12 @@ public class BinanceWrapper {
     private static final String TAG = "BinanceWrapper";
     private static BinanceWrapper mWrapperInstance;
     private final List<MarketUpdateListener> mListener;
-    private BinanceApiRestClient mClient;
-    private BinanceApiAsyncRestClient mAsyncClient;
+    private final BinanceApiWebSocketClient mWebSocketClient;
 
     private BinanceWrapper(Context context) {
         BinanceApiClientFactory factory = BinanceApiClientFactory.newInstance(context.
                 getString(R.string.binance_api_key), context.getString(R.string.binance_api_secret));
-        mClient = factory.newRestClient();
-        mAsyncClient = factory.newAsyncRestClient();
+        mWebSocketClient = factory.newWebSocketClient();
         mListener = new ArrayList<>();
         startListening();
     }
@@ -45,6 +45,17 @@ public class BinanceWrapper {
 
     public void remoteUpdateHandler(MarketUpdateListener listener) {
         mListener.remove(listener);
+    }
+
+    public void startOrderBookListening(String token, final DepthListener listener) {
+        mWebSocketClient.onDepthEvent(token, new BinanceApiCallback<DepthEvent>() {
+            @Override
+            public void onResponse(DepthEvent depthEvent) throws BinanceApiException {
+                if (listener != null) {
+                    listener.onDepthUpdate(depthEvent);
+                }
+            }
+        });
     }
 
     private void startListening() {
@@ -66,5 +77,9 @@ public class BinanceWrapper {
 
     public interface MarketUpdateListener {
         boolean onMarketTickerUpdate(AllMarketTickersEvent event);
+    }
+
+    public interface DepthListener {
+        void onDepthUpdate(DepthEvent event);
     }
 }
